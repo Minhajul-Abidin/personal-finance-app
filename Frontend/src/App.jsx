63125ad77041d739c6a78f9f55b04f3e32d2bab4
Minhajul-Abidin <113845913+Minhajul-Abidin@users.noreply.github.com>
2025-02-09 19:29:52 +0530

    refactor: move Home component to pages directory and update import path

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 
