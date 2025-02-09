f92446e56ecdc547b01585f1b12f740011877bef
Minhajul-Abidin <113845913+Minhajul-Abidin@users.noreply.github.com>
2025-02-09 20:08:24 +0530

    feat: add Profile and Settings pages with routing in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
-
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
         <Navigation />
       </BrowserRouter>
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -2,8 +2,8 @@ import { VscSettings } from "react-icons/vsc";
 import { CgProfile } from "react-icons/cg";
 function Header() {
   return (
-    <div className="flex flex-row justify-between fixed w-full max-h-[4.4rem] px-6 py-3 bg-white shadow-md rounded-b-2xl sm:max-h-[5rem] sm:py-4">
-      <div className="flex flex-row text-2xl">
+    <div className="flex flex-row justify-between w-full max-h-[4.4rem] px-6 py-3 bg-white shadow-md rounded-b-2xl sm:max-h-[5rem] sm:py-4">
+      <div className="flex flex-row text-2xl font-extralight">
         <a href="/">F I N E</a>
       </div>
       <div className="flex flex-row gap-8 text-3xl font-thin">
