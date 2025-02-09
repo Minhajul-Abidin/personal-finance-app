39095fa2e7904d66fa7c5da57d26c000e73c29c4
Minhajul-Abidin <113845913+Minhajul-Abidin@users.noreply.github.com>
2025-02-09 19:57:06 +0530

    style: update Header component with new branding and icon integration

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,12 +1,18 @@
+import { VscSettings } from "react-icons/vsc";
+import { CgProfile } from "react-icons/cg";
 function Header() {
   return (
     <div className="flex flex-row justify-between fixed w-full max-h-[4.4rem] px-6 py-3 bg-white shadow-md rounded-b-2xl sm:max-h-[5rem] sm:py-4">
-      <div className="flex flex-row">
-        <a href="/">Home</a>
+      <div className="flex flex-row text-2xl">
+        <a href="/">F I N E</a>
       </div>
-      <div className="flex flex-row">
-        <a href="/settings">Settings</a>
-        <a href="/profile">Profile</a>
+      <div className="flex flex-row gap-8 text-3xl font-thin">
+        <a href="/settings">
+          <VscSettings />
+        </a>
+        <a href="/profile">
+          <CgProfile />
+        </a>
       </div>
     </div>
   );
